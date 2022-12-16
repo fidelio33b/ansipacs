f16c8c78587794759a85502a64a12d0321d099f1
Laurent Lavaud <laurent.lavaud@developpement-durable.gouv.fr>
2022-12-16 11:44:49 +0100

    installation de python3

--- docker/create_containers.sh
+++ docker/create_containers.sh
@@ -2,7 +2,10 @@
 
 for i in db01 dcm01 rp01
 do
-    docker run -d -t --name pacs-${i} --hostname pacs-${i} ubuntu
+    CONTAINER_NAME=pacs-${i}
+    docker run -d -t --name ${CONTAINER_NAME} --hostname ${CONTAINER_NAME} ubuntu
+    sleep 1
+    docker exec -i ${CONTAINER_NAME} apt update
+    docker exec -i ${CONTAINER_NAME} apt -y upgrade
+    docker exec -i ${CONTAINER_NAME} apt -y install python3-minimal
 done
-
-echo "don't forget to add python3-minimal package"
